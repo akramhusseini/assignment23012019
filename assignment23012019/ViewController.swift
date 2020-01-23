0231683a70ca0100bfd33ab70e79a62cf23200d0
Akram Husseini <husseiniakram@gmail.com>
2020-01-23 08:40:14 +0200

    Initial Commit

---
+++ assignment23012019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment23012019
+//
+//  Created by Akram Samir Husseini on 1/23/20.
+//  Copyright © 2020 Akram Samir Husseini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
